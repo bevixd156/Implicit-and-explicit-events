74002762f0b24cd8c62d0941a727e2dd46000edd
bevixd156 <benjaxd156@gmail.com>
2025-10-12 16:14:01 -0300

    Agregación del layout y clase maps

--- app/src/main/java/com/devst/app/MapsActivity.java
+++ app/src/main/java/com/devst/app/MapsActivity.java
@@ -1,6 +1,40 @@
 package com.devst.app;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_maps);
+
+        EditText edtD = findViewById(R.id.edtDireccion);
+        Button btnBuscar = findViewById(R.id.btnBuscar);
+
+        btnBuscar.setOnClickListener(v -> {
+            String lugar = edtD.getText().toString();
+            if (!lugar.isEmpty()) {
+                Uri gmIntentUri = Uri.parse("geo:0,0?=" + Uri.encode(lugar));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(this, "No se encontró la Aplicación de Google Mpas", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Ingresa una Dirección o Lugar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/devst/app/PruebaActivity.java
+++
@@ -1,6 +0,0 @@
-package com.devst.app;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PruebaActivity extends AppCompatActivity {
-}
