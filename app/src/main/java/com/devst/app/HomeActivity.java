2ba0335b25cdd6c6b74dfb1076ed43acc7aaa405
bevixd156 <benjaxd156@gmail.com>
2025-10-12 17:12:56 -0300

    Agregación de evento explicito de HomeActivity y adaptacion a forma horizontal en activity_maps

--- app/src/main/java/com/devst/app/HomeActivity.java
+++ app/src/main/java/com/devst/app/HomeActivity.java
@@ -77,6 +77,7 @@ public class HomeActivity extends AppCompatActivity {
         Button btnAbrirWeb = findViewById(R.id.btnAbrirWeb);
         Button btnEnviarCorreo = findViewById(R.id.btnEnviarCorreo);
         Button btnCompartir = findViewById(R.id.btnCompartir);
+        Button btnIrMaps = findViewById(R.id.btnIrMaps);
         btnLinterna = findViewById(R.id.btnLinterna);
         Button btnCamara = findViewById(R.id.btnCamara);
         Button btnConfigWifi = findViewById(R.id.btnConfigWifi);
@@ -124,9 +125,6 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(wifi);
         });
 
-
-
-
         //Linterna Inicializamos la camara
 
         camara = (CameraManager) getSystemService(CAMERA_SERVICE);
@@ -167,6 +165,10 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(this, CamaraActivity.class))
         );
 
+        //Evento: Intent explicito
+        btnIrMaps.setOnClickListener(v ->
+                startActivity(new Intent(this, MapsActivity.class))
+        );
     }
 
     //Linterna
--- app/src/main/java/com/devst/app/MapsActivity.java
+++ app/src/main/java/com/devst/app/MapsActivity.java
@@ -20,13 +20,18 @@ public class MapsActivity extends AppCompatActivity {
         EditText edtD = findViewById(R.id.edtDireccion);
         Button btnBuscar = findViewById(R.id.btnBuscar);
 
+        // Definimos las funciones del boton Buscar
         btnBuscar.setOnClickListener(v -> {
+            // Definimos un String para el lugar que el usuario ingresará
             String lugar = edtD.getText().toString();
+            // Si el lugar se encuentra lo que retornará
+            // Será la aplicacion con el lugar ingresado
             if (!lugar.isEmpty()) {
                 Uri gmIntentUri = Uri.parse("geo:0,0?=" + Uri.encode(lugar));
+                // Intent para ingresar el lugar que el usuario busca
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
-
+                // Si el
                 if (mapIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(mapIntent);
                 } else {
